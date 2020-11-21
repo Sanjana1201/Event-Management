e6bfdf00dc00463501762e10cabcb1225dd25966
programmer <63101162+drishtijain01@users.noreply.github.com>
2020-11-21 20:00:16 +0530

    Add files via upload

---
+++ EventManager/add.event.php
@@ -0,0 +1,31 @@
+<?php 
+ 
+ $con = mysqli_connect ('localhost', 'root', '');
+ 
+ if (!$con)
+ {
+	 echo 'not connected to server';
+ }
+ if (!mysqli_select_db($con, 'insertion'))
+ {
+	 echo 'database not selected';
+ }
+
+ $event_name = $_POST['eventname'];
+ 
+ $sql = "INSERT INTO event (event_name) VALUES ('$event_name')";
+
+ if (!mysqli_query ($con, $sql))
+ {
+	 echo 'not inserted';
+ }
+ else 
+ {
+	 echo '<script type="text/javascript">
+                      alert("New Event Added!");
+                         location="home.php";
+                           </script>';
+ }
+
+
+?>
